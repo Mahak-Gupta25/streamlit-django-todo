a3993df7ee881de4d1b461bb2ac896a0988c2344
Mahak <mahak03.gupta@gmail.com>
2022-07-05 15:11:39 +0530

    changes

--- streamlit_django_todo/settings.py
+++ streamlit_django_todo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'todo',
     'crispy_forms',
+    'streamlit-django-todo.herokuapp.com',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+django_heroku.settings(locals())
